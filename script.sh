42c779d628086dfa21e4f1291464dade1c209679
Rudy <rudyalvs@gmail.com>
2024-10-25 12:26:43 +0200

    Normalement ca marche

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+apt remove -y apache2
+apt update -y
 apt install -y apache2
+systemctl enable apache2
 cd /var/www/html
 rm index.html
+wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
