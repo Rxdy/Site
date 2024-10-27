9e0b3fdf850ac49a3883b1eb2aabd1f0cb44599b
Rudy <44189564+Rxdy@users.noreply.github.com>
2024-10-27 18:45:44 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 distrib=$(cat /etc/issue | grep "Debian");
+
 if [ $USER != 'root' ]; 
 then 
   echo "Erreur ! Attention, vous devez être en root pour lancer le script"; 
-  echo "Error ! Please, you must be root to run the script"; 
-  echo $distrib;
+  echo "Error ! Please, you must be root to run the script";
 else
-  if [$distrib == NULL];
+  if [-z $distrib];
   then
     dnf update -y
     dnf install -y httpd
     systemctl enable httpd && systemctl start httpd
     firewall-cmd --add-service=http --permanent
     firewall-cmd --reload
-    cd /var/www/html
-    rm index.html
-    wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
-    echo "Installation terminée";
   else
     apt update -y
     apt install -y apache2
     systemctl enable apache2
-    cd /var/www/html
-    rm index.html
-    wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
-    echo "Installation terminée";
   fi
+  cd /var/www/html
+  rm index.html
+  wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
+  echo "Installation terminée";
 fi
