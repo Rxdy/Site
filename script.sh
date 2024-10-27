30979c3b0e182fbd98455ad156cd44fd39dab441
Rudy <44189564+Rxdy@users.noreply.github.com>
2024-10-27 19:21:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ then
   echo "Error ! Please, you must be root to run the script";
   echo $distrib;
 else
-  if [$distrib == 'ID="rocky"']
+  if [$distrib == 'ID="rocky"'];
   then
     dnf update -y
     dnf install -y httpd
@@ -15,11 +15,11 @@ else
     systemctl enable httpd && systemctl start httpd
     firewall-cmd --add-service=http --permanent
     firewall-cmd --reload
-  elif [$distrib == 'ID=debian']
+  elif [$distrib == 'ID=debian'];
     apt update -y
     apt install -y apache2
     systemctl enable apache2 && systemctl start apache2
-    apt install -y git
+    apt install -y git;
   fi
   cd /var/www/html
   rm -f index.html
