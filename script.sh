681e97f3a21675ec9e55b5641eaece3695270d6b
Rudy <44189564+Rxdy@users.noreply.github.com>
2024-10-27 19:51:49 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,21 +5,25 @@ if [ "$USER" != "root" ]; then
   echo "Error ! Please, you must be root to run the script"
   echo "Distribution détectée : $distrib"
 else
-  if [ "$distrib" == "rocky" ]; then
-  echo "je suis ici"
+  if [ "$distrib" == 'ID="rocky"']; then
+    echo "je suis ici"
     dnf update -y
     dnf install -y httpd
     dnf install -y git
     systemctl enable httpd && systemctl start httpd
     firewall-cmd --add-service=http --permanent
     firewall-cmd --reload
-  elif [ "$distrib" == "debian" ]; then
+  elif [ "$distrib" == 'ID=debian' ]; then
+    echo "je suis ici"
     apt update -y
     apt install -y apache2
     systemctl enable apache2 && systemctl start apache2
     apt install -y git
+  else
+      echo "Cette distribution n'est pas géré"
+      exit
   fi
-  cd /var/www/html || exit
+  cd /var/www/html
   rm -f index.html
   wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
   echo "Installation terminée"
