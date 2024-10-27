4960259912a794f4a2055d3a3c23c21af295fb82
hugolagouardat <45795395+hugolagouardat@users.noreply.github.com>
2024-10-27 19:42:08 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
 distrib=$(cat /etc/*release* | grep "^ID=");
 
-if [ $USER != 'root' ]; 
-then 
-  echo "Erreur ! Attention, vous devez être en root pour lancer le script"; 
-  echo "Error ! Please, you must be root to run the script";
-  echo $distrib;
+if [ "$USER" != "root" ]; then 
+  echo "Erreur ! Attention, vous devez être en root pour lancer le script" 
+  echo "Error ! Please, you must be root to run the script"
+  echo "Distribution détectée : $distrib"
 else
-  if [ $distrib == 'ID="rocky"' ];
-  then
+  if [ "$distrib" == "rocky" ]; then
     dnf update -y
     dnf install -y httpd
     dnf install -y git
     systemctl enable httpd && systemctl start httpd
     firewall-cmd --add-service=http --permanent
     firewall-cmd --reload
-  elif [ $distrib == 'ID=debian' ];
+  elif [ "$distrib" == "debian" ]; then
     apt update -y
     apt install -y apache2
-    systemctl enable apache2
+    systemctl enable apache2 && systemctl start apache2
+    apt install -y git
   fi
-  cd /var/www/html
+  cd /var/www/html || exit
   rm -f index.html
   wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
-  echo "Installation terminée";
+  echo "Installation terminée"
 fi
