f58d44136c86b9c9da7fcb3962e10ce24ffcea06
hugolagouardat <45795395+hugolagouardat@users.noreply.github.com>
2024-10-27 17:57:44 +0100

    Update scriptRocky.sh

--- scriptRocky.sh
+++ scriptRocky.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+if [ $USER != 'root' ]; 
+then 
+  echo "Vous devez être en root pour lancer le script"; 
+else
 dnf update -y
 dnf install -y httpd
 systemctl enable httpd && systemctl start httpd
@@ -8,3 +11,4 @@ firewall-cmd --reload
 cd /var/www/html
 rm index.html
 wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
+fi
