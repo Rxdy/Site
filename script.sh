b150fa9a304c3de9342538d9af1cf32cc18c8159
Rudy <44189564+Rxdy@users.noreply.github.com>
2024-10-27 19:25:24 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ else
 elif [$distrib == 'ID=debian'];
     apt update -y
     apt install -y apache2
-    systemctl enable apache2 && systemctl start apache2
+    systemctl enable apache2
     apt install -y git;
 fi
   cd /var/www/html
