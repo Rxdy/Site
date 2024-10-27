e298ee535faa896f914a3e5c5a20a075c6d5012f
Rudy <44189564+Rxdy@users.noreply.github.com>
2024-10-27 17:55:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-apt update -y
-apt install -y apache2
-systemctl enable apache2
-cd /var/www/html
-rm index.html
-wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
+if [ $USER != 'root' ]; 
+then 
+  echo "Vous devez être en root pour lancer le script"; 
+else
+  apt update -y
+  apt install -y apache2
+  systemctl enable apache2
+  cd /var/www/html
+  rm index.html
+  wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
+fi
