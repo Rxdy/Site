4b3a17e2c868d5fe652039bc8d79e0b2b119ca65
Lucas-ui <116873717+Lucas-ui@users.noreply.github.com>
2024-10-28 21:30:49 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,6 @@ if [ "$USER" != "root" ]; then
   echo "Error ! Please, you must be root to run the script"
 else
   if [ "$distrib" == 'ID="rocky"' ]; then
-    echo "je suis ici"
     dnf update -y
     dnf install -y httpd
     systemctl enable httpd && systemctl start httpd
@@ -19,12 +18,14 @@ else
     systemctl enable apache2 && systemctl start apache2
   else
       echo "Cette distribution n'est pas supportée. Une distribution Rocky Linux ou Debian est demandée."
+      echo "This distribution is not supported. A Rocky Linux or Debian distribution is required."
       exit
   fi
   cd /var/www/html
   rm -f index.html
   wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
-  echo "Installation terminée, pour accéder au site utilisé le lien ci dessous sur votre navigateur."
+  echo "Installation terminée, pour accéder au site, utilisez le lien ci-dessous dans votre navigateur."
+  echo "Installation complete. To access the site, use the link below in your browser."
   echo " "
   echo "---------------------"
   echo "http://"$ipadress
