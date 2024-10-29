abba2808c32ef0e1683cd4d7201d7ab9d8a6c624
Rudy <rudyalvs@gmail.com>
2024-10-29 19:39:43 +0100

    change nom dossier en html

--- script.sh
+++ script.sh
@@ -9,21 +9,25 @@ else
   if [ "$distrib" == 'ID="rocky"' ]; then
     dnf update -y
     dnf install -y httpd
+    dnf install -y git
     systemctl enable httpd && systemctl start httpd
     firewall-cmd --add-service=http --permanent
     firewall-cmd --reload
   elif [ "$distrib" == 'ID=debian' ]; then
     apt update -y
     apt install -y apache2
+    apt install -y git
     systemctl enable apache2 && systemctl start apache2
   else
       echo "Cette distribution n'est pas supportée. Une distribution Rocky Linux ou Debian est demandée."
       echo "This distribution is not supported. A Rocky Linux or Debian distribution is required."
       exit
   fi
-  cd /var/www/html
-  rm -f index.html
-  wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
+  cd /var/www
+  rm -rf html
+  git clone https://github.com/Rxdy/html.git temp-repo
+  cp -r temp-repo/html/* /var/www
+  rm -rf temp-repo
   echo "Installation terminée. Pour accéder au site, utilisez le lien ci-dessous dans votre navigateur."
   echo "Installation complete. To access the site, use the link below in your browser."
   echo " "
