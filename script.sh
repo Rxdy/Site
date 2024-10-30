c7597bc300e8eac0f5d51ef0e6d2d8c94c044657
Rudy <44189564+Rxdy@users.noreply.github.com>
2024-10-30 19:41:30 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 distrib=$(cat /etc/*release* | grep "^ID=");
 ipadress=$(hostname -I | cut -d ' ' -f1);
 path="/var/www/html"
-if [ "$USER" != "root" ]; then 
-  echo " "
+if [ "$USER" != "root" ]; then
   echo "---------------------------------------------------------------------"
   echo "Erreur ! Attention, vous devez être en root pour lancer le script." 
   echo "Error ! Please, you must be root to run the script."
@@ -32,8 +31,9 @@ else
   mkdir lenofo
   cd lenofo
   wget https://raw.githubusercontent.com/Rxdy/Site/main/lenofo.zip
-  unzip lenofo.zip -d $path
+  unzip lenofo.zip -d $path/lenofo
   rm -rf lenofo.zip
+  echo " "
   echo "Installation terminée. Pour accéder au site, utilisez le lien ci-dessous dans votre navigateur."
   echo "Installation complete. To access the site, use the link below in your browser."
   echo " "
