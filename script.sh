fe9beecf9b628764a822135514e5a2a47e028152
olivier.r.jobs@gmail.com <votre.email@exemple.com>
2024-11-02 09:14:31 +0100

    Mise à jour du contenu de la branche olivier

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install -y apache2
-systemctl enable apache2
-cd /var/www/html
-rm index.html
-wget https://raw.githubusercontent.com/Rxdy/Site/refs/heads/main/index.html
